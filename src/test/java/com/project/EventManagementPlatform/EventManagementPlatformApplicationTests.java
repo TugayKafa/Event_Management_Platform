2280c14904527fc2c5136dcb84e1fdb9412fb9ed
Tugay Kafa <kafatugay@gmail.com>
2025-02-05 15:05:08 +0200

    init

---
+++ src/test/java/com/project/EventManagementPlatform/EventManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.EventManagementPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
